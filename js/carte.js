bad02c6c6698ff8281ea2b2237de44c8ff131882
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2018-12-12 16:43:34 +0100

    let

--- js/carte.js
+++ js/carte.js
@@ -1,5 +1,6 @@
+let mymap;
 function showMap(lat,lon){
-var mymap = L.map('mapid').setView([lat,lon], 13);
+mymap = L.map('mapid').setView([lat,lon], 13);
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
 		maxZoom: 18,
