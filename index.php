b377894cb72ee7de40e7431caacb9e44dfdf4c7a
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2018-12-12 14:59:37 +0100

    modifs index

--- index.php
+++ index.php
@@ -14,15 +14,13 @@ function meteo($lat, $lon)
     $str = "http://www.infoclimat.fr/public-api/gfs/xml?_ll=".$lat.",".$lon."&_auth=ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2";
     $file = file_get_contents($str);
     $meteo = simplexml_load_string($file);
-    $xml =  new DOMDocument(); 
-    $xml->load( $file ); 
     $xsl = new DOMDocument;
     $xsl->load("meteo.xsl");
     $proc = new XSLTProcessor;
     $proc->importStyleSheet($xsl);
 
-    echo $proc->transformToXML($xml);
-    echo $meteo->message;
+    echo $proc->transformToXML($meteo);
+
 }
 
   get_contents();
